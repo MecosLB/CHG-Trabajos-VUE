d589110888d5ee841ee2f96c0ece6af8168e748c
Sebastian CHG <sebokiabot@gmail.com>
2024-03-08 16:58:18 -0600

    Usuarios - module finished

---
+++ src/components/dashboard/UsersOptions.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="dropdown">
+        <button class="btn btn-secondary rounded-5" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="fa-solid fa-ellipsis"></i>
+        </button>
+
+        <ul class="dropdown-menu">
+            <li>
+                <button @click="updateTitle" :userId="userId" class="dropdown-item text-primary" data-bs-toggle="modal"
+                    data-bs-target="#userModal">
+                    <i class="fa-solid fa-pencil"></i> Editar
+                </button>
+            </li>
+
+            <li>
+                <button :userId="userId" @click="onUserDelete" class="dropdown-item text-danger">
+                    <i class="fa-solid fa-trash"></i> Eliminar
+                </button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['userId', 'updateTitle', 'onUserDelete']);
+</script>
