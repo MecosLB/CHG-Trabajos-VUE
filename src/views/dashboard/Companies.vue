7832c746a67c438b85a7b10210903217437237fb
Fernando A Jacobo <fernando-chg@outlook.com>
2024-03-11 13:34:49 -0600

    Correción path logotipos

--- src/views/dashboard/Companies.vue
+++ src/views/dashboard/Companies.vue
@@ -100,7 +100,7 @@
 
                         <div class="container-card animate__animated animate__fadeIn">
                             <div class="col-sm-12 col-md-12 col-lg-6 d-flex align-items-center justify-content-center">
-                                <img :id="company.id" :src="`/logotipos/${company.id}.png`" class="img-fluid p-2"
+                                <img :id="company.id" :src="`logotipos/${company.id}.png`" class="img-fluid p-2"
                                     :alt="company.nombre" :title="company.nombre"
                                     @error="setDefaultLogotipo(company.id)">
                             </div>
